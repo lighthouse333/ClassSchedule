f316278e3847a74a4b8d0461cea47999d312e782
lighthouse333 <3404429273@qq.com>
2026-08-16 13:26:18 +0800

    Prepare v1.3.0 release (#16)
    Co-authored-by: lighthouse333 <293710977+lighthouse333@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.example.timetable"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
